fc957b60b91e4733f93ead80d61e9b37f59e7038
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-24 14:33:38 +0530

    Added the template for response error and asynchandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    }
+    catch (error) {
+        res.status(500).json({ message: "internal server error" });
+        console.log("error while processing request", error);
+    }
+}
+export default asyncHandler;
+
