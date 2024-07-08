f3fbce35aa52bf89bd5188ca1e02801e7e85d717
yagyagoel1 <yagyagoel87@gmail.com>
2024-07-08 06:59:20 +0530

    removed the vercel json file

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -17,7 +17,8 @@ const Logger = () => {
 
     transports: [
       new transports.Console(),
-
+      new transports.File({ filename: ".logs/error.log", level: "error" }),
+      new transports.File({ filename: ".logs/combined.log" }),
     ],
   });
 };
