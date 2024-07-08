3c4bbe87d0f9eb95008156e11b9d035706186b31
yagyagoel1 <yagyagoel87@gmail.com>
2024-07-08 06:54:51 +0530

    aremoved the logs folder

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -17,8 +17,7 @@ const Logger = () => {
 
     transports: [
       new transports.Console(),
-      new transports.File({ filename: ".logs/error.log", level: "error" }),
-      new transports.File({ filename: ".logs/combined.log" }),
+
     ],
   });
 };
